3cbd1fb79b098c92fe4509e529f5dd12fa911359
DayZ n' Chill <brian@dayznchill.com>
2024-10-20 23:18:32 -0500

    Reordered my code
    for legibility

--- Colorful-UI/Scripts/3_Game/Colorful-UI/cui.DayZGame.c
+++ Colorful-UI/Scripts/3_Game/Colorful-UI/cui.DayZGame.c
@@ -1,40 +1,50 @@
-// Priority Queue ---------------------------------- --addind to test a push--
-modded class LoginQueueBase extends LoginScreenBase
+// Loading Screen (UH DUH!) --------------------
+modded class LoadingScreen
 {
+    protected ImageWidget m_Background;
     protected ImageWidget m_tShader;
     protected ImageWidget m_bShader;
-    protected ProgressBarWidget m_ProgressLoading;
+    protected ImageWidget m_tipIcon;
+    protected TextWidget m_loadingMsg;
 
-    override Widget Init()
-    {    
-        // Use CUI Layout    
-        layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/gui/layouts/cui.dialog_queue_position.layout");
-        // Vanilla Elements
-        m_HintPanel = new UiHintPanelLoading(layoutRoot.FindAnyWidget("hint_frame0"));
-        m_txtPosition = TextWidget.Cast(layoutRoot.FindAnyWidget("LoadingMsg"));
-        m_txtNote = TextWidget.Cast(layoutRoot.FindAnyWidget("txtNote"));
-        m_btnLeave = ButtonWidget.Cast(layoutRoot.FindAnyWidget("btnLeave"));
+    void LoadingScreen(DayZGame game)
+    {
+        m_DayZGame = game;
+        // Use the CUI 2.0 custom layout 
+        m_WidgetRoot = game.GetLoadingWorkspace().CreateWidgets("Colorful-UI/gui/layouts/cui.loading.layout");
         
-        m_tShader = ImageWidget.Cast(layoutRoot.FindAnyWidget("TopShader"));
-        m_bShader = ImageWidget.Cast(layoutRoot.FindAnyWidget("BottomShader"));
-        m_ProgressLoading = ProgressBarWidget.Cast(layoutRoot.FindAnyWidget("LoadingBar"));
+        // CUI 2.0 Elements
+        Class.CastTo(m_tShader, m_WidgetRoot.FindAnyWidget("TopShader"));
+        Class.CastTo(m_bShader, m_WidgetRoot.FindAnyWidget("BottomShader"));
+        Class.CastTo(m_loadingMsg, m_WidgetRoot.FindAnyWidget("LoadingMsg"));
         
-        m_btnLeave.SetColor(colorScheme.TipIcon());
+        // Load BG and Progress Bar
+        m_Background = ImageWidget.Cast(m_WidgetRoot.FindAnyWidget("ImageBackground"));
+        m_ProgressLoading = ProgressBarWidget.Cast(m_WidgetRoot.FindAnyWidget("LoadingBar"));
+        ProgressAsync.SetProgressData(m_ProgressLoading);
+        ProgressAsync.SetUserData(m_Background);
+        m_Background.Show(true);
+        
+        // Theme the elements. 
+        m_ProgressLoading.SetColor(colorScheme.Loadingbar());
         m_tShader.SetColor(colorScheme.TopShader());
         m_bShader.SetColor(colorScheme.BottomShader());
-        m_ProgressLoading.SetColor(colorScheme.Loadingbar());
-
-        return layoutRoot;
+        m_loadingMsg.SetColor(colorScheme.LoadingMsg());
+        
+        // I plan to make this timed and show some silly loading comments from the community.
+        m_loadingMsg.SetText("GAME IS LOADING!");
     }
 
-    override void SetPosition(int position)
+    override void SetTitle(string title)
     {
-        if (position != m_iPosition)
-        {
-            m_iPosition = position;
-            m_txtPosition.SetText("Position in Queue " + position.ToString());
-            m_txtPosition.SetColor(colorScheme.LoadingMsg());
-        }
+        // This function is intentionally left blank because it is no longer used.
+    }
+
+    override void Show()
+    {
+        // Use Random Background Images
+        m_Background = ImageWidget.Cast(m_WidgetRoot.FindAnyWidget("ImageBackground"));        
+        m_Background.LoadImageFile(0, GetRandomBackground()); 
     }
 };
 
@@ -129,52 +139,42 @@ modded class LoginTimeBase extends LoginScreenBase
     }   
 };
 
-// Loading Screen (UH DUH!) --------------------
-modded class LoadingScreen
+// Priority Queue ----------------------------------
+modded class LoginQueueBase extends LoginScreenBase
 {
-    protected ImageWidget m_Background;
     protected ImageWidget m_tShader;
     protected ImageWidget m_bShader;
-    protected ImageWidget m_tipIcon;
-    protected TextWidget m_loadingMsg;
+    protected ProgressBarWidget m_ProgressLoading;
 
-    void LoadingScreen(DayZGame game)
-    {
-        m_DayZGame = game;
-        // Use the CUI 2.0 custom layout 
-        m_WidgetRoot = game.GetLoadingWorkspace().CreateWidgets("Colorful-UI/gui/layouts/cui.loading.layout");
+    override Widget Init()
+    {    
+        // Use CUI Layout    
+        layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/gui/layouts/cui.dialog_queue_position.layout");
+        // Vanilla Elements
+        m_HintPanel = new UiHintPanelLoading(layoutRoot.FindAnyWidget("hint_frame0"));
+        m_txtPosition = TextWidget.Cast(layoutRoot.FindAnyWidget("LoadingMsg"));
+        m_txtNote = TextWidget.Cast(layoutRoot.FindAnyWidget("txtNote"));
+        m_btnLeave = ButtonWidget.Cast(layoutRoot.FindAnyWidget("btnLeave"));
         
-        // CUI 2.0 Elements
-        Class.CastTo(m_tShader, m_WidgetRoot.FindAnyWidget("TopShader"));
-        Class.CastTo(m_bShader, m_WidgetRoot.FindAnyWidget("BottomShader"));
-        Class.CastTo(m_loadingMsg, m_WidgetRoot.FindAnyWidget("LoadingMsg"));
+        m_tShader = ImageWidget.Cast(layoutRoot.FindAnyWidget("TopShader"));
+        m_bShader = ImageWidget.Cast(layoutRoot.FindAnyWidget("BottomShader"));
+        m_ProgressLoading = ProgressBarWidget.Cast(layoutRoot.FindAnyWidget("LoadingBar"));
         
-        // Load BG and Progress Bar
-        m_Background = ImageWidget.Cast(m_WidgetRoot.FindAnyWidget("ImageBackground"));
-        m_ProgressLoading = ProgressBarWidget.Cast(m_WidgetRoot.FindAnyWidget("LoadingBar"));
-        ProgressAsync.SetProgressData(m_ProgressLoading);
-        ProgressAsync.SetUserData(m_Background);
-        m_Background.Show(true);
-        
-        // Theme the elements. 
-        m_ProgressLoading.SetColor(colorScheme.Loadingbar());
+        m_btnLeave.SetColor(colorScheme.TipIcon());
         m_tShader.SetColor(colorScheme.TopShader());
         m_bShader.SetColor(colorScheme.BottomShader());
-        m_loadingMsg.SetColor(colorScheme.LoadingMsg());
-        
-        // I plan to make this timed and show some silly loading comments from the community.
-        m_loadingMsg.SetText("GAME IS LOADING!");
+        m_ProgressLoading.SetColor(colorScheme.Loadingbar());
+
+        return layoutRoot;
     }
 
-    override void SetTitle(string title)
+    override void SetPosition(int position)
     {
-        // This function is intentionally left blank because it is no longer used.
-    }
-
-    override void Show()
-    {
-        // Use Random Background Images
-        m_Background = ImageWidget.Cast(m_WidgetRoot.FindAnyWidget("ImageBackground"));        
-        m_Background.LoadImageFile(0, GetRandomBackground()); 
+        if (position != m_iPosition)
+        {
+            m_iPosition = position;
+            m_txtPosition.SetText("Position in Queue " + position.ToString());
+            m_txtPosition.SetColor(colorScheme.LoadingMsg());
+        }
     }
 };
