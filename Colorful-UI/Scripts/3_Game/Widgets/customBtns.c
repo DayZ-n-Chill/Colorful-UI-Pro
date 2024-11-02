a7771af6d27bf3e87ea21faa517dbdb246fa3488
DayZ n' Chill <brian@dayznchill.com>
2024-11-02 12:50:25 -0500

    Added Event Handler

--- Colorful-UI/Scripts/3_Game/Widgets/customBtns.c
+++ Colorful-UI/Scripts/3_Game/Widgets/customBtns.c
@@ -1,18 +1,14 @@
-
 class CUIButton {
-    // Converts ARGB values into a single integer representation
     static int ARGB(int alpha, int red, int green, int blue) {
-        return (alpha << 24) | (red << 16) | (green << 8) | blue; // Combine ARGB into a single integer
+        return (alpha << 24) | (red << 16) | (green << 8) | blue; 
     }
 
-    // Sets up a button with text and hover background color
     static void baseBtn(ButtonWidget button, string text, int textColor, int hoverColor) {
         if (!button) return; 
         
         button.SetText(text);
         button.SetTextColor(textColor); 
         button.SetColor(hoverColor); 
-        // This sets the hover just fine for all butons and you dont need a event handler to do the backgroung hovr effcect
 
         string textWidgetName = button.GetName() + "_label"; 
         TextWidget textWidget = TextWidget.Cast(button.FindAnyWidget(textWidgetName));
@@ -22,7 +18,50 @@ class CUIButton {
             textWidget.SetText(text);
             button.SetText("");
         }
+
+        // Attach the handler for hover and click interactions
+        CUIButtonHandler handler = new CUIButtonHandler(button, textColor, hoverColor, hoverColor);
+        button.SetHandler(handler);
     }
 }
 
+class CUIButtonHandler : ScriptedWidgetEventHandler {
+    private ButtonWidget m_Button;
+    private int m_BaseColor;
+    private int m_HoverColor;
+    private int m_TextColor;
 
+    void CUIButtonHandler(ButtonWidget button, int textColor, int baseColor, int hoverColor) {
+        m_Button = button;
+        m_TextColor = textColor;
+        m_BaseColor = baseColor;
+        m_HoverColor = hoverColor;
+
+        // Set initial button properties
+        if (m_Button) {
+            m_Button.SetTextColor(m_TextColor);
+            m_Button.SetColor(m_BaseColor);
+        }
+    }
+
+    override bool OnMouseEnter(Widget w, int x, int y) {
+        if (w == m_Button) {
+            m_Button.SetColor(m_HoverColor);
+        }
+        return true;
+    }
+
+    override bool OnMouseLeave(Widget w, Widget enterW, int x, int y) {
+        if (w == m_Button) {
+            m_Button.SetColor(m_BaseColor);
+        }
+        return true;
+    }
+
+    override bool OnClick(Widget w, int x, int y, int button) {
+        if (w == m_Button && button == MouseState.LEFT) {
+            return true;
+        }
+        return false;
+    }
+}
--- Colorful-UI/Scripts/5_Mission/GUI/NewUI/MainMenu/MainMenu.c
+++ Colorful-UI/Scripts/5_Mission/GUI/NewUI/MainMenu/MainMenu.c
@@ -7,14 +7,14 @@ modded class MainMenu extends UIScriptedMenu {
         layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/gui/layouts/new_ui/testing.layout");
 
         // Find the buttons in your layout
-        m_testBtn = ButtonWidget.Cast(layoutRoot.FindAnyWidget("testBtn"));
+        m_testBtn  = ButtonWidget.Cast(layoutRoot.FindAnyWidget("testBtn"));
         m_testBtn2 = ButtonWidget.Cast(layoutRoot.FindAnyWidget("testBtn2"));
         m_testBtn3 = ButtonWidget.Cast(layoutRoot.FindAnyWidget("testBtn3"));
 
         // Call the new button methods
-        CUIButton.baseBtn(m_testBtn,  "Bitch Boi", UIColor.Violet(), UIColor.Yellow());
-        CUIButton.baseBtn(m_testBtn2, "Bitch Ass", UIColor.Orange(), UIColor.Green());
-        CUIButton.baseBtn(m_testBtn3, "Bitch Ass MF", UIColor.Blue(), UIColor.Blue());
+        CUIButton.baseBtn(m_testBtn,  "Bitch Boi",    UIColor.Violet(), UIColor.Yellow());
+        CUIButton.baseBtn(m_testBtn2, "Bitch Ass",    UIColor.Orange(), UIColor.Green());
+        CUIButton.baseBtn(m_testBtn3, "Bitch Ass MF", UIColor.Blue(),   UIColor.Blue());
 
         return layoutRoot;
     }
