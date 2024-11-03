8cac27cfdac3131f4a45d5be35866994fdb2b3e4
DayZ n' Chill <brian@dayznchill.com>
2024-11-03 12:32:37 -0600

    Fixed colorbase

--- Colorful-UI/Scripts/3_Game/Systems/Managers/colorManger.c
+++ Colorful-UI/Scripts/3_Game/Systems/Managers/colorManger.c
@@ -6,54 +6,49 @@ class UIColor
 	// Many of these colors are included for convenience and may not be necessary 
 	// for your current use. Please feel free to omit any that you don't need.
 
-	// Brand Specific 
-	static int BrandColor()           { return ARGB(255, 255, 204, 102); }   
-	static int AccentColor()          { return ARGB(255, 100, 35, 35); }  
-	static int TertiaryColor()        { return ARGB(255, 204, 255, 204); }
-
 	// GreyScale
-	static int White()                { return ARGB(255, 255, 255, 255); };
-	static int Grey()                 { return ARGB(255, 130, 130, 130); };
-	static int Black()                { return ARGB(255, 0, 0, 0); };
-	static int Transparent()          { return ARGB(0, 0, 0, 0); };
-	static int SemiTransparentWhite() { return ARGB(120, 255, 255, 255); };
+	static int White()            { return ARGB(255, 255, 255, 255); };
+	static int Grey()             { return ARGB(255, 130, 130, 130); };
+	static int Black()            { return ARGB(255, 0, 0, 0); };
+	static int Transparent()      { return ARGB(0, 0, 0, 0); };
+	static int SemiTransWhite()   { return ARGB(120, 255, 255, 255); };
+	static int SemiTransBlack()   { return ARGB(120, 0, 0, 0); };
 	
 	// R.O.Y.G.B.I.V
-	static int Red()    			  { return ARGB(255, 173, 35, 35); };
-	static int Orange() 			  { return ARGB(255, 215, 153, 19); };
-	static int Yellow() 			  { return ARGB(255, 255, 204, 102); };
-	static int Green()  			  { return ARGB(255, 51, 204, 51); };
-	static int Blue()   			  { return ARGB(255, 51, 102, 102); };
-	static int Indigo() 			  { return ARGB(255, 102, 51, 204); };
-	static int Violet() 			  { return ARGB(255, 204, 51, 204); };
+	static int Red()    		  { return ARGB(255, 173, 35, 35); };
+	static int Orange() 		  { return ARGB(255, 215, 153, 19); };
+	static int Yellow() 		  { return ARGB(255, 255, 204, 102); };
+	static int Green()  		  { return ARGB(255, 51, 204, 51); };
+	static int Blue()   		  { return ARGB(255, 51, 102, 102); };
+	static int Indigo() 		  { return ARGB(255, 102, 51, 204); };
+	static int Violet() 		  { return ARGB(255, 204, 51, 204); };
 	
 	// Social Network Colors 
-	static int facebook()              { return ARGB(255, 59, 89, 152); };
-	static int instagram()             { return ARGB(255, 193, 42, 163); };
-	static int linkedin()              { return ARGB(255, 0, 119, 181); };
-	static int pinterest()             { return ARGB(255, 189, 8, 28); };
-	static int snapchat()              { return ARGB(255, 255, 252, 0); };
-	static int tiktok()                { return ARGB(255, 1, 1, 1); };
-	static int whatsapp()              { return ARGB(255, 37, 211, 102); };
-	static int tumblr()                { return ARGB(255, 54, 70, 93); };
-	static int flickr()                { return ARGB(255, 244, 0, 131); };
-	static int vimeo()                 { return ARGB(255, 26, 183, 234); };
-	static int skype()                 { return ARGB(255, 0, 175, 240); };
-	static int slack()                 { return ARGB(255, 74, 21, 75); };
-	static int medium()                { return ARGB(255, 2, 184, 117); };
-	static int quora()                 { return ARGB(255, 170, 34, 0); };
-	static int dribbble()              { return ARGB(255, 234, 76, 137); };
-	static int soundcloud()            { return ARGB(255, 255, 85, 0); };
-	static int spotify()               { return ARGB(255, 30, 215, 96); };
-	static int twitch()                { return ARGB(255, 145, 70, 255); };
-	static int telegram()              { return ARGB(255, 0, 136, 204); };
-	static int mastodon()              { return ARGB(255, 99, 100, 255); };
-
-	// Custom Colors 
-	// Use to add your custom colors and use them where you want.
-	static int cuiTeal()             { return ARGB(255, 102, 153, 153); };
-	static int cuiBlue()             { return ARGB(255, 1, 5, 74); };
-	static int cuiDarkBlue()         { return ARGB(155, 0, 0, 32); };
-	static int cuiSubtleRed()        { return ARGB(255, 100, 35, 35); };
-	static int cuiBrightRed()        { return ARGB(255, 152, 0, 0); };
+	static int facebook()         { return ARGB(255, 59, 89, 152); };
+	static int instagram()        { return ARGB(255, 193, 42, 163); };
+	static int linkedin()         { return ARGB(255, 0, 119, 181); };
+	static int pinterest()        { return ARGB(255, 189, 8, 28); };
+	static int snapchat()         { return ARGB(255, 255, 252, 0); };
+	static int tiktok()           { return ARGB(255, 1, 1, 1); };
+	static int whatsapp()         { return ARGB(255, 37, 211, 102); };
+	static int tumblr()           { return ARGB(255, 54, 70, 93); };
+	static int flickr()           { return ARGB(255, 244, 0, 131); };
+	static int vimeo()            { return ARGB(255, 26, 183, 234); };
+	static int skype()            { return ARGB(255, 0, 175, 240); };
+	static int slack()            { return ARGB(255, 74, 21, 75); };
+	static int medium()           { return ARGB(255, 2, 184, 117); };
+	static int quora()            { return ARGB(255, 170, 34, 0); };
+	static int dribbble()         { return ARGB(255, 234, 76, 137); };
+	static int soundcloud()       { return ARGB(255, 255, 85, 0); };
+	static int spotify()          { return ARGB(255, 30, 215, 96); };
+	static int twitch()           { return ARGB(255, 145, 70, 255); };
+	static int telegram()         { return ARGB(255, 0, 136, 204); };
+	static int mastodon()         { return ARGB(255, 99, 100, 255); };
+ 	
+	//Custom Colors 
+	static int cuiTeal()          { return ARGB(255, 102, 153, 153); };
+	static int cuiBlue()          { return ARGB(255, 1, 5, 74); };
+	static int cuiDarkBlue()      { return ARGB(155, 0, 0, 32); };
+	static int cuiSubtleRed()     { return ARGB(255, 100, 35, 35); };
+	static int cuiBrightRed()     { return ARGB(255, 152, 0, 0); };
 }
--- Colorful-UI/Scripts/3_Game/Systems/Managers/themeManger.c
+++ Colorful-UI/Scripts/3_Game/Systems/Managers/themeManger.c
@@ -2,38 +2,43 @@
 // Adjust these settings to align with your desired theme and aesthetics.
 class colorScheme 
 {
+	// Brand Specific 
+	static int BrandColor()           { return ARGB(255, 255, 204, 102); }   
+	static int AccentColor()          { return ARGB(255, 100, 35, 35); }  
+	static int TertiaryColor()        { return ARGB(255, 204, 255, 204); }
+
 	// Base Typography 
 	static int PrimaryText()         { return UIColor.White(); }           // Primary text color for main body content
 	static int SecondaryText()       { return UIColor.Grey(); }            // Color for secondary text, e.g., subtitles or info
-	static int TextHover()           { return UIColor.BrandColor(); }              // Text color for hover effects, creates interactivity
+	static int TextHover()           { return BrandColor(); }              // Text color for hover effects, creates interactivity
 
 	// Global UI Elements 
-	static int ButtonHover()         { return UIColor.BrandColor(); }              // Button color on hover, provides feedback
-	static int LogOutTimer()         { return UIColor.BrandColor(); }              // Timer color for logout warnings or countdowns
-	static int Separator()           { return UIColor.BrandColor(); }             // Divider color to segment UI sections
-	static int TabHover()            { return UIColor.BrandColor(); }              // Tab color on hover, indicates active navigation
-	static int Loadingbar()          { return UIColor.BrandColor(); }             // Loading bar color for visual indication
+	static int ButtonHover()         { return BrandColor(); }              // Button color on hover, provides feedback
+	static int LogOutTimer()         { return BrandColor(); }              // Timer color for logout warnings or countdowns
+	static int Separator()           { return BrandColor(); }             // Divider color to segment UI sections
+	static int TabHover()            { return BrandColor(); }              // Tab color on hover, indicates active navigation
+	static int Loadingbar()          { return BrandColor(); }             // Loading bar color for visual indication
 
 	// Shader Colors 
-	static int TopShader()           { return UIColor.CUI2Teal(); }        // Top shader on layouts for added depth
-	static int BottomShader()        { return UIColor.CUI2Blue(); }        // Bottom shader on layouts for added depth
+	static int TopShader()           { return UIColor.cuiTeal(); }        // Top shader on layouts for added depth
+	static int BottomShader()        { return UIColor.cuiBlue(); }        // Bottom shader on layouts for added depth
 
 	// Loading Screen
-	static int TipIcon()             { return UIColor.BrandColor(); }              // Icon color for loading tips, aligns with bra   nd
-	static int TipHeader()           { return UIColor.BrandColor(); }              // Header color for tips, separates from body t   ext
-	static int TipLine()             { return UIColor.BrandColor(); }              // Divider line color in tips, adds structure   
-	static int TipText()             { return UIColor.PrimaryText(); }             // Main text color for tips, ensures readabilit   y
-	static int LoadingMsg()          { return UIColor.TertiaryColor(); }           // Color for loading messages, subtle but disti   nct
+	static int TipText()             { return PrimaryText(); }             // Main text color for tips, ensures readabilit   y
+	static int LoadingMsg()          { return TertiaryColor(); }           // Color for loading messages, subtle but disti   nct
+	static int TipIcon()             { return BrandColor(); }              // Icon color for loading tips, aligns with bra   nd
+	static int TipHeader()           { return BrandColor(); }              // Header color for tips, separates from body t   ext
+	static int TipLine()             { return BrandColor(); }              // Divider line color in tips, adds structure   
 
 	// Main Menu
-	static int NavIcon()             { return UIColor.BrandColor(); }              // Navigation icon color, promotes brand consistency
-	static int SurvivorBox()         { return UIColor.CUI2DarkBlue(); }    // Background color for "Survivor" box, adds separation
-	static int StatsBox()            { return UIColor.CUI2DarkBlue(); }    // Background color for "Stats" box, harmonizes with SurvivorBox
+	static int NavIcon()             { return BrandColor(); }              // Navigation icon color, promotes brand consistency
+	static int SurvivorBox()         { return UIColor.cuiDarkBlue(); }       // Background color for "Survivor" box, adds separation
+	static int StatsBox()            { return UIColor.cuiDarkBlue(); }       // Background color for "Stats" box, harmonizes with SurvivorBox
 
 	// Options Page 
-	static int OptionHeaders()       { return UIColor.TextHover(); }               // Header color in options, aligns with hover effect
-	static int OptionInputColors()   { return UIColor.TextHover(); }               // Input fields color, consistent with hover
-	static int OptionSliderColors()  { return UIColor.TextHover(); }              // Slider color for a unified interactive feel
-	static int OptionCaretColors()   { return UIColor.TextHover(); }               // Caret color for dropdowns, harmonizes with inputs
-	static int OptionBGHover()       { return UIColor.TextHover(); }               // Background color for hovered options, reinforces interactivity
+	static int OptionHeaders()       { return BrandColor(); }               // Header color in options, aligns with hover effect
+	static int OptionInputColors()   { return BrandColor(); }               // Input fields color, consistent with hover
+	static int OptionSliderColors()  { return BrandColor(); }              // Slider color for a unified interactive feel
+	static int OptionCaretColors()   { return BrandColor(); }               // Caret color for dropdowns, harmonizes with inputs
+	static int OptionBGHover()       { return BrandColor(); }               // Background color for hovered options, reinforces interactivity
 }
