6c3f555c343882da4ea79fbaa1b8441c07ce6900
DayZ n' Chill <brian@dayznchill.com>
2024-12-27 09:09:03 -0600

    Hint Panel Fixes

--- Colorful-UI/Scripts/3_Game/Helpers/utils.c
+++ Colorful-UI/Scripts/3_Game/Helpers/utils.c
@@ -1,7 +1,8 @@
-// These are utility functions that are used in the mod that you can use in your own U.
 class cuiUtils
 {
-    static int m_RandomIndex = -1; 
+    static int m_RandomIndex = -1;
+
+    // Returns a random index while avoiding consecutive duplicates
     static int GetRandomIndex(int count)
     {
         int newIndex = Math.RandomIntInclusive(0, count - 1);
@@ -13,7 +14,28 @@ class cuiUtils
         return newIndex;
     }
 
+    // Combines ARGB color values into a single integer
     static int ARGB(int alpha, int red, int green, int blue) {
         return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
+
+    // Handles showing or hiding hints based on the condition
+    static void HandleHints(DayZGame game, Widget widgetRoot, bool noHints)
+    {
+        if (!noHints)
+        {
+            UiHintPanelLoading hintPanel = new UiHintPanelLoading(widgetRoot.FindAnyWidget("hint_frame"));
+            hintPanel.Init(game);
+        }
+        else
+        {
+            widgetRoot.FindAnyWidget("hint_frame").Show(false);
+        }
+    }
+
+    // Helper to hide hints explicitly
+    static void NoHints(Widget widgetRoot)
+    {
+        widgetRoot.FindAnyWidget("hint_frame").Show(false);
     }
 };
