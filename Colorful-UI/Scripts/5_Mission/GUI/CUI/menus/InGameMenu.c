06183e02dc6be146168007a9ea056347e2b58aaa
DayZ n' Chill <brian@dayznchill.com>
2025-01-10 17:44:01 -0600

    Fixing buttons.c

--- Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
+++ Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
@@ -1,165 +1,116 @@
 modded class InGameMenu extends UIScriptedMenu
 {
-	private Widget m_Separator;
-
-	private Widget cui_Discord;
-	private Widget cui_Twitter;
-	private Widget cui_Youtube;
-	private Widget cui_Reddit;
-	private Widget cui_Facebook;
-	private Widget m_MenuDivider;
-	
-	private Widget m_TopShader;
-	private Widget m_BottomShader;
-
+	// private Widget m_Separator;
+	protected ImageWidget m_TopShader, m_BottomShader, m_MenuDivider;
+	protected ButtonWidget m_PrioQ, m_Website, m_Discord, m_Twitter, m_Youtube, m_Reddit, m_Facebook;	
+	float m_TimerSlice;
 	private Widget m_GameOverScreen;
 	private ImageWidget m_GameOverScreenImage;
-	float m_TimerSlice;
+	
 
 	override Widget Init()
 	{
 		layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/GUI/layouts/menus/inGame/cui.ingame.layout");
+		m_HintPanel					= new UiHintPanel(layoutRoot.FindAnyWidget( "hint_frame" ));
 			
 		m_ContinueButton			= layoutRoot.FindAnyWidget( "ContinueBtn" );
 		m_ExitButton				= layoutRoot.FindAnyWidget( "ExitBtn" );
-		m_MenuDivider				= layoutRoot.FindAnyWidget( "MenuDivider" );
 		m_RestartButton				= layoutRoot.FindAnyWidget( "RestartBtn" );
 		m_RespawnButton 			= layoutRoot.FindAnyWidget( "respawn_button" );
 		m_RestartDeadRandomButton	= layoutRoot.FindAnyWidget( "respawn_button_random" );
 		m_RestartDeadCustomButton	= layoutRoot.FindAnyWidget( "respawn_button_custom" );
 		m_OptionsButton				= layoutRoot.FindAnyWidget( "OptionsBtn" );
-		
-
-		m_HintPanel					= new UiHintPanel(layoutRoot.FindAnyWidget( "hint_frame" ));
-		
-		// m_ServerInfoPanel 			= layoutRoot.FindAnyWidget( "server_info" );
-		// m_ServerIP 					= TextWidget.Cast( layoutRoot.FindAnyWidget( "server_ip" ) );
-		// m_ServerPort 				= TextWidget.Cast( layoutRoot.FindAnyWidget( "server_port" ) );
-		// m_ServerName 				= TextWidget.Cast( layoutRoot.FindAnyWidget( "server_name" ) );
-		// m_FavoriteImage 			= layoutRoot.FindAnyWidget( "favorite_image" );
-		// m_UnfavoriteImage 			= layoutRoot.FindAnyWidget( "unfavorite_image" );
-		// m_CopyInfoButton 			= layoutRoot.FindAnyWidget( "copy_button" );
-		// m_Separator 				= layoutRoot.FindAnyWidget( "actionItems_separator" );
-	
-		cui_Discord 				= layoutRoot.FindAnyWidget( "DiscordBtn" );
-		cui_Twitter 				= layoutRoot.FindAnyWidget( "TwitterBtn" );
-		cui_Youtube 				= layoutRoot.FindAnyWidget( "YoutubeBtn" );
-		cui_Reddit 					= layoutRoot.FindAnyWidget( "RedditBtn" );
-		cui_Facebook 				= layoutRoot.FindAnyWidget( "FacebookBtn" );
+		m_PrioQ						= layoutRoot.FindAnyWidget( "QueueBtn" );
+		m_Website					= layoutRoot.FindAnyWidget( "WebsiteBtn" );
+		m_MenuDivider				= layoutRoot.FindAnyWidget( "MenuDivider" );
+		m_Discord 					= layoutRoot.FindAnyWidget( "DiscordBtn" );
+		m_Twitter 					= layoutRoot.FindAnyWidget( "TwitterBtn" );
+		m_Youtube 					= layoutRoot.FindAnyWidget( "YoutubeBtn" );
+		m_Reddit 					= layoutRoot.FindAnyWidget( "RedditBtn" );
+		m_Facebook 					= layoutRoot.FindAnyWidget( "FacebookBtn" );
 
 		m_TopShader 			    = layoutRoot.FindAnyWidget( "TopShader" );
-		m_BottomShader 			    = layoutRoot.FindAnyWidget( "BottomShader" );
+		m_BottomShader 				= layoutRoot.FindAnyWidget( "BottomShader" );
 
 		m_MenuDivider.SetColor(colorScheme.Separator());
 		m_TopShader.SetColor(colorScheme.TopShader());
 		m_BottomShader.SetColor(colorScheme.BottomShader());
 
 		HudShow( false );
+		
+		cuiElmnt.proBtn(m_ContinueButton, "Continue", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		cuiElmnt.proBtn(m_OptionsButton, "Options", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		cuiElmnt.proBtn(m_ExitButton, "Exit", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		cuiElmnt.proBtn(m_RestartButton, "Restart", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		cuiElmnt.proBtn(m_RestartDeadRandomButton, "Random Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		cuiElmnt.proBtn(m_RestartDeadCustomButton, "Custom Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+		
+		// cuiElmnt.proBtn(
+		// 	m_RespawnButton,
+		// 	"Respawn",
+		// 	colorScheme.PrimaryText(),
+		// 	colorScheme.ButtonHover(),
+		// 	OnClick_Respawn(),
+		// );
+		
+		cuiElmnt.proBtn(
+			m_PrioQ,
+			"Priority Queue",
+			colorScheme.PrimaryText(),
+			colorScheme.ButtonHover(),
+			MenuURLS.urlPriorityQ,
+		);
+	
+		cuiElmnt.proBtn(
+			m_Website,
+			"Visit Website",
+			colorScheme.PrimaryText(),
+			UIColor.Facebook(),
+			MenuURLS.urlFacebook,
+		);
 
+		cuiElmnt.proBtn(
+			m_Discord,
+			"Discord",
+			colorScheme.PrimaryText(),
+			UIColor.Discord(),
+			MenuURLS.urlDiscord,
+		);
 
-		m_GameOverScreen = Widget.Cast(layoutRoot.FindAnyWidget("GameOverScreen"));
-		m_GameOverScreen.SetAlpha(0);
-		m_GameOverScreen.Show(false);
-		m_GameOverScreenImage = ImageWidget.Cast(m_GameOverScreen.FindAnyWidget("GameOverScreenImage"));
-		m_GameOverScreenImage.LoadImageFile(0, GameOverScreens.GetRandomElement());
-		m_GameOverScreenImage.SetAlpha(0);
+		cuiElmnt.proBtn(
+			m_Twitter,
+			"Twitter",
+			colorScheme.PrimaryText(),
+			UIColor.Twitter(),
+			MenuURLS.urlTwitter,
+		);	
 
-		cuiElmnt.proBtn(cui_Discord, "Discord", colorScheme.PrimaryText(), UIColor.Discord());
-		cuiElmnt.proBtn(cui_Twitter, "Twitter", colorScheme.PrimaryText(), UIColor.Twitter());
-		cuiElmnt.proBtn(cui_Youtube, "Youtube", colorScheme.PrimaryText(), UIColor.YouTube());
-		cuiElmnt.proBtn(cui_Reddit, "Reddit", colorScheme.PrimaryText(), UIColor.Reddit());
-		cuiElmnt.proBtn(cui_Facebook, "Facebook", colorScheme.PrimaryText(), UIColor.Facebook());
+		cuiElmnt.proBtn(
+			m_Youtube,
+			"Youtube",
+			colorScheme.PrimaryText(),
+			UIColor.YouTube(),
+			MenuURLS.urlYoutube,
+		);
+
+		cuiElmnt.proBtn(
+			m_Reddit,
+			"Reddit",
+			colorScheme.PrimaryText(),
+			UIColor.Reddit(),
+			MenuURLS.urlReddit,
+		);
+
+		cuiElmnt.proBtn(
+			m_Facebook,
+			"Facebook",
+			colorScheme.PrimaryText(),
+			UIColor.Facebook(),
+			MenuURLS.urlFacebook,
+		);
 
 		return layoutRoot;
 	}
-
-
-	// override void Update(float timeslice)
-	// {
-	// 	if (ShowDeadScreen)
-	// 	{
-	// 		m_TimerSlice += timeslice;
-	// 		if (m_TimerSlice >= 0.01)
-	// 		{
-	// 			GameOverMan(timeslice);
-	// 			m_TimerSlice = 0;
-	// 		}
-	// 	}
-	// 	super.Update(timeslice);
-	// };
-
-	// void GameOverMan(float timeslice)
-	// {
-	// 	if (m_GameOverScreenImage.GetAlpha() < 1)
-	// 	{
-	// 		m_GameOverScreen.Show(true);
-
-	// 		float newAlpha = Math.Min(m_GameOverScreen.GetAlpha() + (1.5 * timeslice), 1);
-	// 		m_GameOverScreen.SetAlpha(newAlpha);
-
-	// 		if (newAlpha > 0.5)
-	// 		{
-	// 			m_GameOverScreenImage.SetAlpha(Math.Min(m_GameOverScreenImage.GetAlpha() + (1.25 * timeslice), 1));
-	// 		}
-	// 	}
-	// }
-
-	// override bool OnClick(Widget w, int x, int y, int button)
-	// {
-	// 	if ( w == m_ContinueButton )
-	// 	{
-	// 		OnClick_Continue();
-	// 		return true;
-	// 	}
-	// 	else if ( w == m_RestartButton )
-	// 	{
-	// 		OnClick_Restart();
-	// 		return true;
-	// 	}
-	// 	else if ( w == m_RespawnButton )
-	// 	{
-	// 		OnClick_Respawn();
-	// 		return true;
-	// 	}
-	// 	else if ( w == m_OptionsButton )
-	// 	{
-	// 		OnClick_Options();
-	// 		return true;
-	// 	}
-	// 	else if ( w == m_ExitButton )
-	// 	{
-	// 		OnClick_Exit();
-	// 		return true;
-	// 	}
-	// 	else if (button == MouseState.LEFT && w == cui_Discord)
-	// 	{
-	// 		GetGame().OpenURL(MenuURLS.urlDiscord);
-	// 		return true;
-	// 	}
-	// 	else if (button == MouseState.LEFT && w == cui_Twitter)
-	// 	{
-	// 		GetGame().OpenURL(MenuURLS.urlTwitter);
-	// 		return false;
-	// 	}
-	// 	else if (button == MouseState.LEFT && w == cui_Reddit)
-	// 	{
-	// 		GetGame().OpenURL(MenuURLS.urlReddit);
-	// 		return false;
-	// 	}
-	// 	else if (button == MouseState.LEFT && w == cui_Youtube)
-	// 	{
-	// 		GetGame().OpenURL(MenuURLS.urlYoutube);
-	// 		return false;
-	// 	}	
-	// 	else if (button == MouseState.LEFT && w == cui_Facebook)
-	// 	{
-	// 		GetGame().OpenURL(MenuURLS.urlFacebook);
-	// 		return false;
-	// 	}
-
-	// 	return false;
-	// }
-
 	
 	override void OnShow() {
 	    super.OnShow();
--- Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
+++ Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
@@ -7,106 +7,106 @@ class CUIButtonHandler
     private int m_TextColor;
     private int m_HoverColor;
 
+    private string m_ClickAction; // To store the action or URL
+    private Class m_TargetClass;  // For method callbacks
+    private string m_CallbackMethod;
+
     void CUIButtonHandler(
         ButtonWidget button,
         TextWidget textWidget,
         ImageWidget imageWidget,
         int textColor,
-        int hoverColor
-    )
-    {
+        int hoverColor,
+        string clickAction = "",
+        Class targetClass = null,
+        string callbackMethod = "",
+    ) {
         m_Button = button;
         m_TextWidget = textWidget;
         m_ImageWidget = imageWidget;
         m_TextColor = textColor;
         m_HoverColor = hoverColor;
+        m_ClickAction = clickAction;
+        m_TargetClass = targetClass;
+        m_CallbackMethod = callbackMethod;
 
         ApplyBaseStyles();
     }
 
-    private void ApplyBaseStyles()
-    {
-        if (!m_TextWidget && !m_ImageWidget)
-        {
+    private void ApplyBaseStyles() {
+        if (!m_TextWidget && !m_ImageWidget) {
             m_Button.SetTextColor(m_TextColor);
             return;
         }
 
-        if (m_TextWidget)
-        {
+        if (m_TextWidget) {
             m_TextWidget.SetColor(m_TextColor);
-        }
-        else
-        {
+        } else {
             m_Button.SetTextColor(m_TextColor);
         }
 
-        if (m_ImageWidget)
-        {
+        if (m_ImageWidget) {
             m_ImageWidget.SetColor(m_HoverColor);
             m_ImageWidget.SetImage(1);
         }
     }
 
-    private void ApplyHoverStyles()
-    {
-        if (!m_TextWidget && !m_ImageWidget)
-        {
+    private void ApplyHoverStyles() {
+        if (!m_TextWidget && !m_ImageWidget) {
             m_Button.SetColor(m_HoverColor);
             return;
         }
 
-        if (m_TextWidget)
-        {
+        if (m_TextWidget) {
             m_TextWidget.SetColor(m_HoverColor);
         }
 
-        if (m_ImageWidget)
-        {
+        if (m_ImageWidget) {
             m_ImageWidget.SetColor(m_HoverColor);
         }
 
         m_Button.SetColor(UIColor.Transparent());
     }
 
-    bool OnMouseEnter(Widget w, int x, int y)
-    {
-        if (w == m_Button)
-        {
+    bool OnMouseEnter(Widget w, int x, int y) {
+        if (w == m_Button) {
             ApplyHoverStyles();
         }
         return true;
     }
 
-    bool OnMouseLeave(Widget w, Widget enterW, int x, int y)
-    {
-        if (w == m_Button)
-        {
+    bool OnMouseLeave(Widget w, Widget enterW, int x, int y) {
+        if (w == m_Button) {
             ApplyBaseStyles();
         }
         return true;
     }
 
-    bool OnFocus(Widget w, int x, int y)
-    {
-        if (w == m_Button)
-        {
+    bool OnFocus(Widget w, int x, int y) {
+        if (w == m_Button) {
             ApplyHoverStyles();
         }
         return true;
     }
 
-    bool OnFocusLost(Widget w, int x, int y)
-    {
-        if (w == m_Button)
-        {
+    bool OnFocusLost(Widget w, int x, int y) {
+        if (w == m_Button) {
             ApplyBaseStyles();
         }
         return true;
     }
 
-    bool OnClick(Widget w, int x, int y, int button)
-    {
+    bool OnClick(Widget w, int x, int y, int button) {
+        if (w == m_Button) {
+            if (m_ClickAction != "") {
+                // Open URL or handle string-based action
+                GetGame().OpenURL(m_ClickAction);
+            } else if (m_TargetClass && m_CallbackMethod != "") {
+                // Trigger callback function
+                GetGame().GameScript.CallFunction(m_TargetClass, m_CallbackMethod, null, 0);
+            }
+            return true;
+        }
         return false;
     }
 }
@@ -115,15 +115,13 @@ class cuiElmnt
 {
     static ref array<ref CUIButtonHandler> s_Handlers = new array<ref CUIButtonHandler>();
 
-    static void proBtn(ButtonWidget button, string text, int textColor, int hoverColor)
-    {
+    static void proBtn(ButtonWidget button, string text, int textColor, int hoverColor, string clickAction = "", Class targetClass = null, string callbackMethod = "") {
         if (!button) return;
 
         button.SetText(text);
 
         TextWidget textWidget = TextWidget.Cast(button.FindAnyWidget(button.GetName() + "_label"));
-        if (textWidget)
-        {
+        if (textWidget) {
             textWidget.SetText(text);
             button.SetText("");
         }
@@ -131,11 +129,7 @@ class cuiElmnt
         ImageWidget imageWidget = ImageWidget.Cast(button.FindAnyWidget(button.GetName() + "_img"));
 
         CUIButtonHandler handler = new CUIButtonHandler(
-            button,
-            textWidget,
-            imageWidget,
-            textColor,
-            hoverColor,
+            button, textWidget, imageWidget, textColor, hoverColor, clickAction, targetClass, callbackMethod,
         );
 
         WidgetEventHandler.GetInstance().RegisterOnMouseEnter(button, handler, "OnMouseEnter");
