340db6b734e27f61aaeee301d88ceb9995fe4438
DayZ n' Chill <brian@dayznchill.com>
2025-01-17 00:35:55 -0600

    Removed Comments

--- Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
+++ Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
@@ -1,7 +1,3 @@
-/*************************************
- * FILE: buttons.c
- *************************************/
-
 class CUIButtonHandler
 {
     private ButtonWidget m_Button;
@@ -11,8 +7,8 @@ class CUIButtonHandler
     private int    m_TextColor;
     private int    m_HoverColor;
 
-    private string m_ClickAction;   // URL or string-based action
-    private Class  m_TargetClass;   // For method callbacks
+    private string m_ClickAction;
+    private Class  m_TargetClass;
     private string m_CallbackMethod;
 
     void CUIButtonHandler(
@@ -41,23 +37,19 @@ class CUIButtonHandler
     {
         if (!m_TextWidget && !m_ImageWidget)
         {
-            // No child text or image: color the button text
             m_Button.SetTextColor(m_TextColor);
             return;
         }
 
-        // If there's a TextWidget child, color that
         if (m_TextWidget)
         {
             m_TextWidget.SetColor(m_TextColor);
         }
         else
         {
-            // Otherwise color the button text
             m_Button.SetTextColor(m_TextColor);
         }
 
-        // If there's an ImageWidget child, set a hover color & image index
         if (m_ImageWidget)
         {
             m_ImageWidget.SetColor(m_HoverColor);
@@ -69,23 +61,20 @@ class CUIButtonHandler
     {
         if (!m_TextWidget && !m_ImageWidget)
         {
-            // No child: color the whole button
             m_Button.SetColor(m_HoverColor);
             return;
         }
 
-        // Otherwise color the text child
         if (m_TextWidget)
         {
             m_TextWidget.SetColor(m_HoverColor);
         }
-        // And/or the image child
+
         if (m_ImageWidget)
         {
             m_ImageWidget.SetColor(m_HoverColor);
         }
 
-        // Make the button background transparent if it has child text/image
         m_Button.SetColor(UIColor.Transparent());
     }
 
@@ -129,12 +118,10 @@ class CUIButtonHandler
     {
         if (w == m_Button)
         {
-            // 1) If we have a clickAction string, open it as a URL
             if (m_ClickAction != "")
             {
                 GetGame().OpenURL(m_ClickAction);
             }
-            // 2) Otherwise, if we have a target + callback, call that function
             else if (m_TargetClass && m_CallbackMethod != "")
             {
                 GetGame().GameScript.CallFunction(m_TargetClass, m_CallbackMethod, null, 0);
@@ -160,10 +147,8 @@ class cuiElmnt
     ) {
         if (!button) return;
 
-        // Set text on the button (if no separate label)
         button.SetText(text);
 
-        // If there's a child TextWidget named "<ButtonName>_label"
         TextWidget textWidget = TextWidget.Cast(button.FindAnyWidget(button.GetName() + "_label"));
         if (textWidget)
         {
@@ -171,10 +156,8 @@ class cuiElmnt
             button.SetText("");
         }
 
-        // If there's a child ImageWidget named "<ButtonName>_img"
         ImageWidget imageWidget = ImageWidget.Cast(button.FindAnyWidget(button.GetName() + "_img"));
 
-        // Create the event handler
         CUIButtonHandler handler = new CUIButtonHandler(
             button, 
             textWidget, 
@@ -186,15 +169,12 @@ class cuiElmnt
             callbackMethod,
         );
 
-        // Register event callbacks
         WidgetEventHandler.GetInstance().RegisterOnMouseEnter(button,    handler, "OnMouseEnter");
         WidgetEventHandler.GetInstance().RegisterOnMouseLeave(button,    handler, "OnMouseLeave");
         WidgetEventHandler.GetInstance().RegisterOnFocus(button,         handler, "OnFocus");
         WidgetEventHandler.GetInstance().RegisterOnFocusLost(button,     handler, "OnFocusLost");
         WidgetEventHandler.GetInstance().RegisterOnClick(button,         handler, "OnClick");
 
-        // Keep a reference so it won’t be garbage-collected
         s_Handlers.Insert(handler);
     }
 }
-
