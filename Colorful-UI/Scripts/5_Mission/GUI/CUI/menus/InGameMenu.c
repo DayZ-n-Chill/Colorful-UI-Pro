c67a0372125c0e756123121b42786173384fd6c9
DayZ n' Chill <brian@dayznchill.com>
2025-01-16 23:10:15 -0600

    Buttons now have callbacks

--- Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
+++ Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
@@ -1,118 +1,197 @@
+/*************************************
+ * FILE: InGameMenu.c (or wherever you place it)
+ *************************************/
+
+// “modded” so it overrides the existing DayZ InGameMenu
 modded class InGameMenu extends UIScriptedMenu
 {
-	// private Widget m_Separator;
-	protected ImageWidget m_TopShader, m_BottomShader, m_MenuDivider;
-	protected ButtonWidget m_PrioQ, m_Website, m_Discord, m_Twitter, m_Youtube, m_Reddit, m_Facebook;	
-	float m_TimerSlice;
-	private Widget m_GameOverScreen;
-	private ImageWidget m_GameOverScreenImage;
-	
+    // Some extra references (as in your original code)
+    protected ImageWidget m_TopShader, m_BottomShader, m_MenuDivider;
+    protected ButtonWidget m_PrioQ, m_Website, m_Discord, m_Twitter, m_Youtube, m_Reddit, m_Facebook;
+    float m_TimerSlice;
+    private Widget m_GameOverScreen;
+    private ImageWidget m_GameOverScreenImage;
 
-	override Widget Init()
-	{
-		layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/GUI/layouts/menus/inGame/cui.ingame.layout");
-		m_HintPanel					= new UiHintPanel(layoutRoot.FindAnyWidget( "hint_frame" ));
-			
-		m_ContinueButton			= layoutRoot.FindAnyWidget( "ContinueBtn" );
-		m_ExitButton				= layoutRoot.FindAnyWidget( "ExitBtn" );
-		m_RestartButton				= layoutRoot.FindAnyWidget( "RestartBtn" );
-		m_RespawnButton 			= layoutRoot.FindAnyWidget( "respawn_button" );
-		m_RestartDeadRandomButton	= layoutRoot.FindAnyWidget( "respawn_button_random" );
-		m_RestartDeadCustomButton	= layoutRoot.FindAnyWidget( "respawn_button_custom" );
-		m_OptionsButton				= layoutRoot.FindAnyWidget( "OptionsBtn" );
-		m_PrioQ						= layoutRoot.FindAnyWidget( "QueueBtn" );
-		m_Website					= layoutRoot.FindAnyWidget( "WebsiteBtn" );
-		m_MenuDivider				= layoutRoot.FindAnyWidget( "MenuDivider" );
-		m_Discord 					= layoutRoot.FindAnyWidget( "DiscordBtn" );
-		m_Twitter 					= layoutRoot.FindAnyWidget( "TwitterBtn" );
-		m_Youtube 					= layoutRoot.FindAnyWidget( "YoutubeBtn" );
-		m_Reddit 					= layoutRoot.FindAnyWidget( "RedditBtn" );
-		m_Facebook 					= layoutRoot.FindAnyWidget( "FacebookBtn" );
+    // These are from vanilla InGameMenu
+    override Widget Init()
+    {
+        // EXACT LAYOUT PATH as in your code
+        layoutRoot = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/GUI/layouts/menus/inGame/cui.ingame.layout");
+        m_HintPanel = new UiHintPanel(layoutRoot.FindAnyWidget("hint_frame"));
 
-		m_TopShader 			    = layoutRoot.FindAnyWidget( "TopShader" );
-		m_BottomShader 				= layoutRoot.FindAnyWidget( "BottomShader" );
+        // Example references from vanilla dayz, plus your custom ones
+        m_ContinueButton          = layoutRoot.FindAnyWidget("ContinueBtn");
+        m_ExitButton             = layoutRoot.FindAnyWidget("ExitBtn");
+        m_RestartButton          = layoutRoot.FindAnyWidget("RestartBtn");
+        m_RespawnButton          = layoutRoot.FindAnyWidget("respawn_button");
+        m_RestartDeadRandomButton= layoutRoot.FindAnyWidget("respawn_button_random");
+        m_RestartDeadCustomButton= layoutRoot.FindAnyWidget("respawn_button_custom");
+        m_OptionsButton          = layoutRoot.FindAnyWidget("OptionsBtn");
+        m_PrioQ                  = layoutRoot.FindAnyWidget("QueueBtn");
+        m_Website                = layoutRoot.FindAnyWidget("WebsiteBtn");
+        m_MenuDivider            = ImageWidget.Cast(layoutRoot.FindAnyWidget("MenuDivider"));
+        m_Discord                = layoutRoot.FindAnyWidget("DiscordBtn");
+        m_Twitter                = layoutRoot.FindAnyWidget("TwitterBtn");
+        m_Youtube                = layoutRoot.FindAnyWidget("YoutubeBtn");
+        m_Reddit                 = layoutRoot.FindAnyWidget("RedditBtn");
+        m_Facebook               = layoutRoot.FindAnyWidget("FacebookBtn");
 
-		m_MenuDivider.SetColor(colorScheme.Separator());
-		m_TopShader.SetColor(colorScheme.TopShader());
-		m_BottomShader.SetColor(colorScheme.BottomShader());
+        m_TopShader              = ImageWidget.Cast(layoutRoot.FindAnyWidget("TopShader"));
+        m_BottomShader           = ImageWidget.Cast(layoutRoot.FindAnyWidget("BottomShader"));
 
-		HudShow( false );
-		
-		cuiElmnt.proBtn(m_ContinueButton, "Continue", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		cuiElmnt.proBtn(m_OptionsButton, "Options", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		cuiElmnt.proBtn(m_ExitButton, "Exit", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		cuiElmnt.proBtn(m_RestartButton, "Restart", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		cuiElmnt.proBtn(m_RestartDeadRandomButton, "Random Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		cuiElmnt.proBtn(m_RestartDeadCustomButton, "Custom Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
-		
-		// cuiElmnt.proBtn(
-		// 	m_RespawnButton,
-		// 	"Respawn",
-		// 	colorScheme.PrimaryText(),
-		// 	colorScheme.ButtonHover(),
-		// 	OnClick_Respawn(),
-		// );
-		
-		cuiElmnt.proBtn(
-			m_PrioQ,
-			"Priority Queue",
-			colorScheme.PrimaryText(),
-			colorScheme.ButtonHover(),
-			MenuURLS.urlPriorityQ,
-		);
-	
-		cuiElmnt.proBtn(
-			m_Website,
-			"Visit Website",
-			colorScheme.PrimaryText(),
-			UIColor.Facebook(),
-			MenuURLS.urlFacebook,
-		);
+        // Example color scheme usage
+        m_MenuDivider.SetColor(colorScheme.Separator());
+        m_TopShader.SetColor(colorScheme.TopShader());
+        m_BottomShader.SetColor(colorScheme.BottomShader());
 
-		cuiElmnt.proBtn(
-			m_Discord,
-			"Discord",
-			colorScheme.PrimaryText(),
-			UIColor.Discord(),
-			MenuURLS.urlDiscord,
-		);
+        HudShow(false);
 
-		cuiElmnt.proBtn(
-			m_Twitter,
-			"Twitter",
-			colorScheme.PrimaryText(),
-			UIColor.Twitter(),
-			MenuURLS.urlTwitter,
-		);	
+        // -----------------------------------------------------------
+        // *** Buttons Setup with proBtn ***
+        // -----------------------------------------------------------
 
-		cuiElmnt.proBtn(
-			m_Youtube,
-			"Youtube",
-			colorScheme.PrimaryText(),
-			UIColor.YouTube(),
-			MenuURLS.urlYoutube,
-		);
+        // 1) Without callback or URL
+        //    (If you just want the button to do nothing but hover-color)
+        cuiElmnt.proBtn(m_ContinueButton, "Continue", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+        cuiElmnt.proBtn(m_OptionsButton,  "Options",  colorScheme.PrimaryText(), colorScheme.ButtonHover());
+        cuiElmnt.proBtn(m_RestartButton,  "Restart",  colorScheme.PrimaryText(), colorScheme.ButtonHover());
+        cuiElmnt.proBtn(m_RestartDeadRandomButton, "Random Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+        cuiElmnt.proBtn(m_RestartDeadCustomButton, "Custom Respawn", colorScheme.PrimaryText(), colorScheme.ButtonHover());
 
-		cuiElmnt.proBtn(
-			m_Reddit,
-			"Reddit",
-			colorScheme.PrimaryText(),
-			UIColor.Reddit(),
-			MenuURLS.urlReddit,
-		);
+        // -----------------------------------------------------------
+        // 2) The m_ExitButton gets a callback
+        //    (Remove or comment out the redundant call if you had it before!)
+        //
+        // ORIGINAL (redundant) call—COMMENTED OUT:
+        // cuiElmnt.proBtn(m_ExitButton, "Exit", colorScheme.PrimaryText(), colorScheme.ButtonHover());
+        //
+        // CORRECT: one call with callback
+        cuiElmnt.proBtn(
+            m_ExitButton,
+            "Exit",
+            colorScheme.PrimaryText(),
+            colorScheme.ButtonHover(),
+            "",          // No URL
+            this,        // targetClass => this InGameMenu instance
+            "OnClick_Exit",  // callbackMethod => function name below
+        );
 
-		cuiElmnt.proBtn(
-			m_Facebook,
-			"Facebook",
-			colorScheme.PrimaryText(),
-			UIColor.Facebook(),
-			MenuURLS.urlFacebook,
-		);
+        // -----------------------------------------------------------
+        // 3) Example of a button that opens a URL
+        cuiElmnt.proBtn(
+            m_PrioQ,
+            "Priority Queue",
+            colorScheme.PrimaryText(),
+            colorScheme.ButtonHover(),
+            MenuURLS.urlPriorityQ,  // Non-empty => will do OpenURL(...)
+        );
 
-		return layoutRoot;
-	}
-	
-	override void OnShow() {
-	    super.OnShow();
-	}
+        cuiElmnt.proBtn(
+            m_Website,
+            "Visit Website",
+            colorScheme.PrimaryText(),
+            UIColor.Facebook(),    // or any color
+            MenuURLS.urlFacebook,
+        );
+
+        cuiElmnt.proBtn(
+            m_Discord,
+            "Discord",
+            colorScheme.PrimaryText(),
+            UIColor.Discord(),
+            MenuURLS.urlDiscord,
+        );
+
+        cuiElmnt.proBtn(
+            m_Twitter,
+            "Twitter",
+            colorScheme.PrimaryText(),
+            UIColor.Twitter(),
+            MenuURLS.urlTwitter,
+        );  
+
+        cuiElmnt.proBtn(
+            m_Youtube,
+            "Youtube",
+            colorScheme.PrimaryText(),
+            UIColor.YouTube(),
+            MenuURLS.urlYoutube,
+        );
+
+        cuiElmnt.proBtn(
+            m_Reddit,
+            "Reddit",
+            colorScheme.PrimaryText(),
+            UIColor.Reddit(),
+            MenuURLS.urlReddit,
+        );
+
+        cuiElmnt.proBtn(
+            m_Facebook,
+            "Facebook",
+            colorScheme.PrimaryText(),
+            UIColor.Facebook(),
+            MenuURLS.urlFacebook,
+        );
+
+        return layoutRoot;
+    }
+
+    // // -----------------------------------------------------------
+    // // Example callback function for the exit button
+    // protected void OnClick_Exit()
+    // {
+    //     Print("OnClick_Exit() called! Logging out now...");
+    //     GetGame().LogoutRequestTime();
+    //     GetGame().GetCallQueue(CALL_CATEGORY_GUI).Call(GetGame().GetMission().CreateLogoutMenu, this);
+    // }
+
+    // override void OnShow()
+    // {
+    //     super.OnShow();
+    //     // any additional logic upon showing the menu
+    // }
+
+    // -----------------------------------------------------------
+    // Example override OnClick (typical DayZ style).
+    // This is from your snippet, but keep in mind your
+    // CUIButtonHandler also intercepts OnClick. They can coexist,
+    // but usually you rely on one system or the other.
+    // override bool OnClick(Widget w, int x, int y, int button)
+    // {
+    //     super.OnClick(w, x, y, button);
+
+    //     if (w == m_ContinueButton)
+    //     {
+    //         Print("Clicked continue => do something or OnClick_Continue");
+    //         return true;
+    //     }
+    //     else if (w == m_RestartButton)
+    //     {
+    //         Print("Clicked restart => do OnClick_Restart or something else");
+    //         return true;
+    //     }
+    //     else if (w == m_RespawnButton)
+    //     {
+    //         Print("Clicked respawn => do OnClick_Respawn");
+    //         return true;
+    //     }
+    //     else if (w == m_OptionsButton)
+    //     {
+    //         Print("Clicked options => OnClick_Options");
+    //         return true;
+    //     }
+    //     else if (w == m_ExitButton)
+    //     {
+    //         // We also have the callback above, but you can do it here:
+    //         OnClick_Exit();
+    //         return true;
+    //     }
+    //     else
+    //     {
+    //         // ...
+    //     }
+
+    //     return false;
+    // }
 }
--- Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
+++ Colorful-UI/Scripts/5_Mission/GUI/Components/buttons.c
@@ -1,14 +1,18 @@
+/*************************************
+ * FILE: buttons.c
+ *************************************/
+
 class CUIButtonHandler
 {
     private ButtonWidget m_Button;
-    private TextWidget m_TextWidget;
-    private ImageWidget m_ImageWidget;
+    private TextWidget   m_TextWidget;
+    private ImageWidget  m_ImageWidget;
 
-    private int m_TextColor;
-    private int m_HoverColor;
+    private int    m_TextColor;
+    private int    m_HoverColor;
 
-    private string m_ClickAction; // To store the action or URL
-    private Class m_TargetClass;  // For method callbacks
+    private string m_ClickAction;   // URL or string-based action
+    private Class  m_TargetClass;   // For method callbacks
     private string m_CallbackMethod;
 
     void CUIButtonHandler(
@@ -17,92 +21,122 @@ class CUIButtonHandler
         ImageWidget imageWidget,
         int textColor,
         int hoverColor,
-        string clickAction = "",
-        Class targetClass = null,
-        string callbackMethod = "",
+        string clickAction     = "",
+        Class  targetClass     = null,
+        string callbackMethod  = ""
     ) {
-        m_Button = button;
-        m_TextWidget = textWidget;
-        m_ImageWidget = imageWidget;
-        m_TextColor = textColor;
-        m_HoverColor = hoverColor;
-        m_ClickAction = clickAction;
-        m_TargetClass = targetClass;
+        m_Button         = button;
+        m_TextWidget     = textWidget;
+        m_ImageWidget    = imageWidget;
+        m_TextColor      = textColor;
+        m_HoverColor     = hoverColor;
+        m_ClickAction    = clickAction;
+        m_TargetClass    = targetClass;
         m_CallbackMethod = callbackMethod;
 
         ApplyBaseStyles();
     }
 
-    private void ApplyBaseStyles() {
-        if (!m_TextWidget && !m_ImageWidget) {
+    private void ApplyBaseStyles()
+    {
+        if (!m_TextWidget && !m_ImageWidget)
+        {
+            // No child text or image: color the button text
             m_Button.SetTextColor(m_TextColor);
             return;
         }
 
-        if (m_TextWidget) {
+        // If there's a TextWidget child, color that
+        if (m_TextWidget)
+        {
             m_TextWidget.SetColor(m_TextColor);
-        } else {
+        }
+        else
+        {
+            // Otherwise color the button text
             m_Button.SetTextColor(m_TextColor);
         }
 
-        if (m_ImageWidget) {
+        // If there's an ImageWidget child, set a hover color & image index
+        if (m_ImageWidget)
+        {
             m_ImageWidget.SetColor(m_HoverColor);
             m_ImageWidget.SetImage(1);
         }
     }
 
-    private void ApplyHoverStyles() {
-        if (!m_TextWidget && !m_ImageWidget) {
+    private void ApplyHoverStyles()
+    {
+        if (!m_TextWidget && !m_ImageWidget)
+        {
+            // No child: color the whole button
             m_Button.SetColor(m_HoverColor);
             return;
         }
 
-        if (m_TextWidget) {
+        // Otherwise color the text child
+        if (m_TextWidget)
+        {
             m_TextWidget.SetColor(m_HoverColor);
         }
-
-        if (m_ImageWidget) {
+        // And/or the image child
+        if (m_ImageWidget)
+        {
             m_ImageWidget.SetColor(m_HoverColor);
         }
 
+        // Make the button background transparent if it has child text/image
         m_Button.SetColor(UIColor.Transparent());
     }
 
-    bool OnMouseEnter(Widget w, int x, int y) {
-        if (w == m_Button) {
+    bool OnMouseEnter(Widget w, int x, int y)
+    {
+        if (w == m_Button)
+        {
             ApplyHoverStyles();
         }
         return true;
     }
 
-    bool OnMouseLeave(Widget w, Widget enterW, int x, int y) {
-        if (w == m_Button) {
+    bool OnMouseLeave(Widget w, Widget enterW, int x, int y)
+    {
+        if (w == m_Button)
+        {
             ApplyBaseStyles();
         }
         return true;
     }
 
-    bool OnFocus(Widget w, int x, int y) {
-        if (w == m_Button) {
+    bool OnFocus(Widget w, int x, int y)
+    {
+        if (w == m_Button)
+        {
             ApplyHoverStyles();
         }
         return true;
     }
 
-    bool OnFocusLost(Widget w, int x, int y) {
-        if (w == m_Button) {
+    bool OnFocusLost(Widget w, int x, int y)
+    {
+        if (w == m_Button)
+        {
             ApplyBaseStyles();
         }
         return true;
     }
 
-    bool OnClick(Widget w, int x, int y, int button) {
-        if (w == m_Button) {
-            if (m_ClickAction != "") {
-                // Open URL or handle string-based action
+    bool OnClick(Widget w, int x, int y, int button)
+    {
+        if (w == m_Button)
+        {
+            // 1) If we have a clickAction string, open it as a URL
+            if (m_ClickAction != "")
+            {
                 GetGame().OpenURL(m_ClickAction);
-            } else if (m_TargetClass && m_CallbackMethod != "") {
-                // Trigger callback function
+            }
+            // 2) Otherwise, if we have a target + callback, call that function
+            else if (m_TargetClass && m_CallbackMethod != "")
+            {
                 GetGame().GameScript.CallFunction(m_TargetClass, m_CallbackMethod, null, 0);
             }
             return true;
@@ -115,29 +149,52 @@ class cuiElmnt
 {
     static ref array<ref CUIButtonHandler> s_Handlers = new array<ref CUIButtonHandler>();
 
-    static void proBtn(ButtonWidget button, string text, int textColor, int hoverColor, string clickAction = "", Class targetClass = null, string callbackMethod = "") {
+    static void proBtn(
+        ButtonWidget button,
+        string text,
+        int textColor,
+        int hoverColor,
+        string clickAction    = "",
+        Class  targetClass    = null,
+        string callbackMethod = ""
+    ) {
         if (!button) return;
 
+        // Set text on the button (if no separate label)
         button.SetText(text);
 
+        // If there's a child TextWidget named "<ButtonName>_label"
         TextWidget textWidget = TextWidget.Cast(button.FindAnyWidget(button.GetName() + "_label"));
-        if (textWidget) {
+        if (textWidget)
+        {
             textWidget.SetText(text);
             button.SetText("");
         }
 
+        // If there's a child ImageWidget named "<ButtonName>_img"
         ImageWidget imageWidget = ImageWidget.Cast(button.FindAnyWidget(button.GetName() + "_img"));
 
+        // Create the event handler
         CUIButtonHandler handler = new CUIButtonHandler(
-            button, textWidget, imageWidget, textColor, hoverColor, clickAction, targetClass, callbackMethod,
+            button, 
+            textWidget, 
+            imageWidget, 
+            textColor, 
+            hoverColor, 
+            clickAction, 
+            targetClass, 
+            callbackMethod
         );
 
-        WidgetEventHandler.GetInstance().RegisterOnMouseEnter(button, handler, "OnMouseEnter");
-        WidgetEventHandler.GetInstance().RegisterOnMouseLeave(button, handler, "OnMouseLeave");
-        WidgetEventHandler.GetInstance().RegisterOnFocus(button, handler, "OnFocus");
-        WidgetEventHandler.GetInstance().RegisterOnFocusLost(button, handler, "OnFocusLost");
-        WidgetEventHandler.GetInstance().RegisterOnClick(button, handler, "OnClick");
+        // Register event callbacks
+        WidgetEventHandler.GetInstance().RegisterOnMouseEnter(button,    handler, "OnMouseEnter");
+        WidgetEventHandler.GetInstance().RegisterOnMouseLeave(button,    handler, "OnMouseLeave");
+        WidgetEventHandler.GetInstance().RegisterOnFocus(button,         handler, "OnFocus");
+        WidgetEventHandler.GetInstance().RegisterOnFocusLost(button,     handler, "OnFocusLost");
+        WidgetEventHandler.GetInstance().RegisterOnClick(button,         handler, "OnClick");
 
+        // Keep a reference so it won’t be garbage-collected
         s_Handlers.Insert(handler);
     }
 }
+
