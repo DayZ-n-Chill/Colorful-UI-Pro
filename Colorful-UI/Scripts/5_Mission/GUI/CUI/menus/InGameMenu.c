fcf656561e414b86d0a4f2151103422a670727cd
DayZ n' Chill <brian@dayznchill.com>
2025-01-27 09:06:26 -0600

    Added Comments

--- Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
+++ Colorful-UI/Scripts/5_Mission/GUI/CUI/menus/InGameMenu.c
@@ -2,6 +2,7 @@ modded class InGameMenu extends UIScriptedMenu
 {
     protected ImageWidget m_TopShader, m_BottomShader, m_MenuDivider;
     protected ButtonWidget m_PrioQ, m_Website, m_Discord, m_Twitter, m_Youtube, m_Reddit, m_Facebook;
+	protected Widget m_TopSpacer, m_BottomSpacer;
 
     override Widget Init()
     {
@@ -24,39 +25,43 @@ modded class InGameMenu extends UIScriptedMenu
 
         m_TopShader         = ImageWidget.Cast(layoutRoot.FindAnyWidget("TopShader"));
         m_BottomShader      = ImageWidget.Cast(layoutRoot.FindAnyWidget("BottomShader"));
-		m_MenuDivider       = ImageWidget.Cast(layoutRoot.FindAnyWidget("MenuDivider"));
-
-        if (m_TopShader)    m_TopShader.SetColor(colorScheme.TopShader());
-        if (m_BottomShader) m_BottomShader.SetColor(colorScheme.BottomShader());
-        if (m_MenuDivider)  m_MenuDivider.SetColor(colorScheme.Separator());
+		
+        m_TopSpacer         = layoutRoot.FindAnyWidget("TopSpacer");
+        m_MenuDivider       = ImageWidget.Cast(layoutRoot.FindAnyWidget("MenuDivider"));
+        m_BottomSpacer      = layoutRoot.FindAnyWidget("BottomSpacer");
    
-        cuiElmnt.proBtn(m_ExitButton,"#main_menu_exit", colorScheme.PrimaryText(), colorScheme.ButtonHover(),"", this,"OnClick_Exit");
-        cuiElmnt.proBtn(m_ContinueButton,"#main_menu_continue",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Continue");
-        cuiElmnt.proBtn(m_OptionsButton,"#layout_xbox_ingame_menu_options",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Options");
-        cuiElmnt.proBtn(m_RestartButton,"#main_menu_restart",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Restart");
-        cuiElmnt.proBtn(m_RespawnButton,"#main_menu_respawn",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Respawn");
+        // Buttons with a callbacks
+        cuiElmnt.proBtnCB(m_ExitButton,"#main_menu_exit", colorScheme.PrimaryText(), colorScheme.ButtonHover(),"", this,"OnClick_Exit");
+        cuiElmnt.proBtnCB(m_ContinueButton,"#main_menu_continue",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Continue");
+        cuiElmnt.proBtnCB(m_OptionsButton,"#layout_xbox_ingame_menu_options",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Options");
+        cuiElmnt.proBtnCB(m_RestartButton,"#main_menu_restart",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Restart");
+        cuiElmnt.proBtnCB(m_RespawnButton,"#main_menu_respawn",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"",this,"OnClick_Respawn");
 
+		// Custom Links
         cuiElmnt.proBtn(m_PrioQ,"Priority Queue",colorScheme.PrimaryText(),colorScheme.ButtonHover(),CustomURL.PriorityQ);           
         cuiElmnt.proBtn(m_Website,"Visit Website",colorScheme.PrimaryText(),colorScheme.ButtonHover(),CustomURL.Website);
 
+		// Social Links
         cuiElmnt.proBtn(m_Discord,"Discord",colorScheme.PrimaryText(),UIColor.Discord(),SocialURL.Discord);
         cuiElmnt.proBtn(m_Twitter,"Twitter",colorScheme.PrimaryText(),UIColor.Twitter(),SocialURL.Twitter);  
         cuiElmnt.proBtn(m_Youtube,"Youtube",colorScheme.PrimaryText(),UIColor.YouTube(),SocialURL.Youtube);
         cuiElmnt.proBtn(m_Reddit,"Reddit",colorScheme.PrimaryText(),UIColor.Reddit(),SocialURL.Reddit);
         cuiElmnt.proBtn(m_Facebook,"Facebook",colorScheme.PrimaryText(),UIColor.Facebook(),SocialURL.Facebook);
 
-        CheckURL(m_PrioQ,    CustomURL.PriorityQ);
-        CheckURL(m_Website,  CustomURL.Website);
-
-        CheckURL(m_Discord,  SocialURL.Discord);
-        CheckURL(m_Twitter,  SocialURL.Twitter);
-        CheckURL(m_Youtube,  SocialURL.Youtube);
-        CheckURL(m_Reddit,   SocialURL.Reddit);
-        CheckURL(m_Facebook, SocialURL.Facebook);
+		// These checks show/hide what buttons that are invalid or null
+        CheckURL(m_PrioQ,    	 CustomURL.PriorityQ);
+        CheckURL(m_Website,  	 CustomURL.Website);
+        CheckSocials(m_Discord,  SocialURL.Discord);
+        CheckSocials(m_Twitter,  SocialURL.Twitter);
+        CheckSocials(m_Youtube,  SocialURL.Youtube);
+        CheckSocials(m_Reddit,   SocialURL.Reddit);
+        CheckSocials(m_Facebook, SocialURL.Facebook);
 
         if (allInvalid && m_MenuDivider)
         {
+            m_TopSpacer.Show(false);
             m_MenuDivider.Show(false);
+            m_BottomSpacer.Show(false);
         }
 
 		HudShow(false);       
