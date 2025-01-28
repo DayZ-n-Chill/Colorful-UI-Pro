32f02a5d585b00df9ebf501836999337c615474c
DayZ n' Chill <brian@dayznchill.com>
2025-01-27 19:33:48 -0600

    Start Dialogs

--- Colorful-UI/Scripts/5_Mission/GUI/Components/UIHandler.c
+++ Colorful-UI/Scripts/5_Mission/GUI/Components/UIHandler.c
@@ -2,6 +2,7 @@ class cuiElmnt
 {
     static ref array<ref CUIButtonHandler> s_Handlers = new array<ref CUIButtonHandler>();
 
+    // Pro Buttons ---------------------------------------------------------------
     static void proBtn(ButtonWidget button, string text, int textColor, int hoverColor,
                        string clickAction = "", Class targetClass = null, string callbackMethod = "",
                        string serverIP = "", int serverPort = 0)
@@ -39,4 +40,46 @@ class cuiElmnt
     {
         proBtn(button, text, textColor, hoverColor, "", targetClass, callbackMethod, "", 0);
     }
+
+
+    // Pro Dialogs ---------------------------------------------------------------
+    // static CUIDialogMenu proDialog(string title, string body,
+    //                             string okText, int okTextColor, int okHoverColor,
+    //                             Class okTargetClass, string okCallbackMethod,
+    //                             string cancelText, int cancelTextColor, int cancelHoverColor,
+    //                             Class cancelTargetClass, string cancelCallbackMethod)
+    // {
+    //     // same steps: create CUIDialogMenu, set title/body
+    //     CUIDialogMenu dialogMenu = CUIDialogMenu.Cast(GetGame().GetUIManager().CreateScriptedMenu(MENU_NONE, null));
+    //     if (!dialogMenu) return null;
+
+    //     dialogMenu.SetTitle(title);
+    //     dialogMenu.SetBody(body);
+
+    //     // OK button
+    //     proBtnCB(
+    //         dialogMenu.GetButtonOK(),
+    //         okText,
+    //         okTextColor,
+    //         okHoverColor,
+    //         okTargetClass,
+    //         okCallbackMethod
+    //     );
+
+    //     // Cancel button
+    //     proBtnCB(
+    //         dialogMenu.GetButtonCancel(),
+    //         cancelText,
+    //         cancelTextColor,
+    //         cancelHoverColor,
+    //         cancelTargetClass,
+    //         cancelCallbackMethod
+    //     );
+
+    //     GetGame().GetUIManager().ShowScriptedMenu(dialogMenu, null);
+    //     return dialogMenu;
+    // }
+
+
 }
+
